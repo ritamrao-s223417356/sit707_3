6e6af8dccc6c71022a00a20d020f72fe6e8d889d
ritamrao <ritam22001@gmail.com>
2024-04-11 13:45:00 +1000

    added more test cases

--- src/test/java/sit707_tasks/DateUtilTest.java
+++ src/test/java/sit707_tasks/DateUtilTest.java
@@ -225,8 +225,287 @@ public class DateUtilTest {
 	}
 
 	
+	@Test
+	public void test1A() {
+	    DateUtil date = new DateUtil(1, 6, 1994);
+	    System.out.println("1A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(31, date.getDay());
+	    Assert.assertEquals(5, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test2A() {
+	    DateUtil date = new DateUtil(2, 6, 1994);
+	    System.out.println("2A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(1, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test3A() {
+	    DateUtil date = new DateUtil(15, 6, 1994);
+	    System.out.println("3A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test4A() {
+	    DateUtil date = new DateUtil(30, 6, 1994);
+	    System.out.println("4A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(29, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test5A() {
+	    DateUtil date = new DateUtil(31, 6, 1994);
+	    System.out.println("5A > " + date);
+	    // This test should throw an exception for an invalid date
+	    Assert.assertThrows(IllegalArgumentException.class, date::decrement);
+	}
+
+	@Test
+	public void test6A() {
+	    DateUtil date = new DateUtil(15, 1, 1994);
+	    System.out.println("6A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(1, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test7A() {
+	    DateUtil date = new DateUtil(15, 2, 1994);
+	    System.out.println("7A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test8A() {
+	    DateUtil date = new DateUtil(15, 11, 1994);
+	    System.out.println("8A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(11, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test9A() {
+	    DateUtil date = new DateUtil(15, 12, 1994);
+	    System.out.println("9A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(12, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test10A() {
+	    DateUtil date = new DateUtil(15, 6, 1700);
+	    System.out.println("10A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1700, date.getYear());
+	}
+
+	@Test
+	public void test11A() {
+	    DateUtil date = new DateUtil(15, 6, 1701);
+	    System.out.println("11A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1701, date.getYear());
+	}
+
+	@Test
+	public void test12A() {
+	    DateUtil date = new DateUtil(15, 6, 2023);
+	    System.out.println("12A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(2023, date.getYear());
+	}
+
+	@Test
+	public void test13A() {
+	    DateUtil date = new DateUtil(15, 6, 2024);
+	    System.out.println("13A > " + date);
+	    date.decrement();
+	    System.out.println(date);
+	    Assert.assertEquals(14, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(2024, date.getYear());
+	}
+
+	@Test
+	public void test1B() {
+	    DateUtil date = new DateUtil(1, 6, 1994);
+	    System.out.println("1B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(2, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test2B() {
+	    DateUtil date = new DateUtil(2, 6, 1994);
+	    System.out.println("2B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(3, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test3B() {
+	    DateUtil date = new DateUtil(15, 6, 1994);
+	    System.out.println("3B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test4B() {
+	    DateUtil date = new DateUtil(30, 6, 1994);
+	    System.out.println("4B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(1, date.getDay());
+	    Assert.assertEquals(7, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test5B() {
+	    DateUtil date = new DateUtil(31, 6, 1994);
+	    System.out.println("5B > " + date);
+	    // This test should throw an exception for an invalid date
+	    Assert.assertThrows(IllegalArgumentException.class, date::increment);
+	}
+
+	@Test
+	public void test6B() {
+	    DateUtil date = new DateUtil(15, 1, 1994);
+	    System.out.println("6B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(1, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test7B() {
+	    DateUtil date = new DateUtil(15, 2, 1994);
+	    System.out.println("7B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test8B() {
+	    DateUtil date = new DateUtil(15, 11, 1994);
+	    System.out.println("8B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(11, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test9B() {
+	    DateUtil date = new DateUtil(15, 12, 1994);
+	    System.out.println("9B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(12, date.getMonth());
+	    Assert.assertEquals(1994, date.getYear());
+	}
+
+	@Test
+	public void test10B() {
+	    DateUtil date = new DateUtil(15, 6, 1700);
+	    System.out.println("10B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1700, date.getYear());
+	}
+
+	@Test
+	public void test11B() {
+	    DateUtil date = new DateUtil(15, 6, 1701);
+	    System.out.println("11B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(1701, date.getYear());
+	}
+
+	@Test
+	public void test12B() {
+	    DateUtil date = new DateUtil(15, 6, 2023);
+	    System.out.println("12B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(2023, date.getYear());
+	}
+
+	@Test
+	public void test13B() {
+	    DateUtil date = new DateUtil(15, 6, 2024);
+	    System.out.println("13B > " + date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(16, date.getDay());
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(2024, date.getYear());
+	}
 
 
-	// Write tests for the remaining months of the year 2024 similarly
+	
 
 }
